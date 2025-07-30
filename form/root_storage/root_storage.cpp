a35ff367895613bc011da3f27962d5ddd45b9e6d
Peter Van Gemmeren <gemmeren@cern.ch>
2025-07-30 21:33:01 +0200

    Remove unneeded implementation

--- form/root_storage/root_storage.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright (C) 2025 ...
-
-#include "root_storage.hpp"
-
-// Factory function implementation
-//std::unique_ptr<IStorage> createStorage() { return std::unique_ptr<IStorage>(new ROOT_Storage()); }
