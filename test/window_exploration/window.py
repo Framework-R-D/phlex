8293cdbfb802e660188322fd8643a7b89aef7372
Marc Paterno <paterno@fnal.gov>
2025-08-08 17:01:29 -0500

    Introduce window_generator

--- test/window_exploration/window.py
+++ test/window_exploration/window.py
@@ -1,5 +1,5 @@
 from itertools import zip_longest
-
+from collections import defaultdict
 
 # TODO: make_tuples relies on the fact that it is operating
 # on a sequence for which it only has to associated adjacent
@@ -33,3 +33,44 @@ def window(f, *, window_generator=make_pairs, xs):
     """
     seq = window_generator(xs)
     return (f(*t) for t in seq)
+
+def window_generator(xs, matcher):
+    """Generate a sequence of windows from the input.
+
+    This version is very limited. It only creates pairs (not longer tuples).
+    It implements a "me and the one after me" window, meaning that the item with
+    the "first" label appears only once, and never in the second position in the
+    pair.
+
+    Args:
+        xs: a generator that yields the input elements.
+        matcher: a predicate that takes two labels and determines whether they match.
+
+    Yields:
+        A tuple of elements from the input.
+        Elements of each tuple will be adjacent, with regards to their labels.
+        The second element of the tuple may be None.
+    """
+    cache = []  # cache of elements
+    use_count = defaultdict(int)  # dict of index -> use count
+    for x in xs:
+        for y in cache:
+            if matcher(x.label, y.label):
+                if x.label < y.label:
+                    yield (x, y)
+                else:
+                    yield (y, x)
+                use_count[x.label] += 1
+                use_count[y.label] += 1
+                if use_count[y.label] == 2:
+                    cache.remove(y)  # relies on __eq__ for the element type
+        if use_count[x.label] < 2:
+            cache.append(x)
+
+    # No more elements are coming; flush the cache.
+    # For each element in the cache, we know that there is not "next" element to
+    # be found later, so we yield (element, None).
+    for element in cache:
+        # We want to yield
+        if element.label != 0:
+            yield (element, None)
