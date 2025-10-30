7fcb88bb5802712b2d9a8f2fd1db95a90bd964c2
Wim Lavrijsen <WLavrijsen@lbl.gov>
2025-10-30 10:45:10 -0700

    extend supported types to a couple more builins and retrieve information from annotations

--- test/python/modulewrap.cpp
+++ test/python/modulewrap.cpp
@@ -82,20 +82,6 @@ namespace {
     }
   };
 
-  // converters
-  static intptr_t pyint(int i)
-  {
-    PyGILRAII gil;
-    return (intptr_t)PyLong_FromLong(i);
-  }
-  static int intpy(intptr_t pyobj)
-  {
-    PyGILRAII gil;
-    int i = (int)PyLong_AsLong((PyObject*)pyobj);
-    Py_DECREF(pyobj);
-    return i;
-  }
-
   static std::vector<std::string> cseq(PyObject* coll)
   {
     size_t len = (size_t)PySequence_Size(coll);
@@ -124,6 +110,113 @@ namespace {
 
 } // unnamed namespace
 
+namespace {
+
+  static std::string annotation_as_text(PyObject* pyobj)
+  {
+    char const* cstr = nullptr;
+    if (!PyUnicode_Check(pyobj)) {
+      PyObject* pystr = PyObject_GetAttrString(pyobj, "__name__"); // eg. for classes
+      if (!pystr) {
+        PyErr_Clear();
+        pystr = PyObject_Str(pyobj);
+      }
+
+      cstr = PyUnicode_AsUTF8(pystr);
+      Py_DECREF(pystr);
+      return cstr;
+    }
+    cstr = PyUnicode_AsUTF8(pyobj);
+
+    if (!cstr)
+      return "";
+    return cstr;
+  }
+
+  // converters of builtin types; TODO: this is a basic subset only, b/c either
+  // these will be generated from the IDL, or they should be picked up from an
+  // existing place such as cppyy
+
+  static bool pylong_as_bool(PyObject* pyobject)
+  {
+    // range-checking python integer to C++ bool conversion
+    long l = PyLong_AsLong(pyobject);
+    // fail to pass float -> bool; the problem is rounding (0.1 -> 0 -> False)
+    if (!(l == 0 || l == 1) || PyFloat_Check(pyobject)) {
+      PyErr_SetString(PyExc_ValueError, "boolean value should be bool, or integer 1 or 0");
+      return (bool)-1;
+    }
+    return (bool)l;
+  }
+
+  static long pylong_as_strictlong(PyObject* pyobject)
+  {
+    // convert <pybject> to C++ long, don't allow truncation
+    if (!PyLong_Check(pyobject)) {
+      PyErr_SetString(PyExc_TypeError, "int/long conversion expects an integer object");
+      return (long)-1;
+    }
+    return (long)PyLong_AsLong(pyobject); // already does long range check
+  }
+
+  static unsigned long pylong_or_int_as_ulong(PyObject* pyobject)
+  {
+    // convert <pybject> to C++ unsigned long, with bounds checking, allow int -> ulong.
+    if (PyFloat_Check(pyobject)) {
+      PyErr_SetString(PyExc_TypeError, "can\'t convert float to unsigned long");
+      return (unsigned long)-1;
+    }
+
+    unsigned long ul = PyLong_AsUnsignedLong(pyobject);
+    if (ul == (unsigned long)-1 && PyErr_Occurred() && PyLong_Check(pyobject)) {
+      PyErr_Clear();
+      long i = PyLong_AS_LONG(pyobject);
+      if (0 <= i) {
+        ul = (unsigned long)i;
+      } else {
+        PyErr_SetString(PyExc_ValueError, "can\'t convert negative value to unsigned long");
+        return (unsigned long)-1;
+      }
+    }
+
+    return ul;
+  }
+
+#define BASIC_CONVERTER(name, cpptype, topy, frompy)                                               \
+  static intptr_t name##_to_py(cpptype a)                                                          \
+  {                                                                                                \
+    PyGILRAII gil;                                                                                 \
+    return (intptr_t)topy(a);                                                                      \
+  }                                                                                                \
+                                                                                                   \
+  static cpptype py_to_##name(intptr_t pyobj)                                                      \
+  {                                                                                                \
+    PyGILRAII gil;                                                                                 \
+    cpptype i = (cpptype)frompy((PyObject*)pyobj);                                                 \
+    Py_DECREF(pyobj);                                                                              \
+    return i;                                                                                      \
+  }
+
+  BASIC_CONVERTER(bool, bool, PyBool_FromLong, pylong_as_bool)
+  BASIC_CONVERTER(int, int, PyLong_FromLong, PyLong_AsLong)
+  BASIC_CONVERTER(uint, unsigned int, PyLong_FromLong, pylong_or_int_as_ulong)
+  BASIC_CONVERTER(long, long, PyLong_FromLong, pylong_as_strictlong)
+  BASIC_CONVERTER(ulong, unsigned long, PyLong_FromUnsignedLong, pylong_or_int_as_ulong)
+  BASIC_CONVERTER(float, float, PyFloat_FromDouble, PyFloat_AsDouble)
+  BASIC_CONVERTER(double, double, PyFloat_FromDouble, PyFloat_AsDouble)
+
+} // unnamed namespace
+
+#define INSERT_INPUT_CONVERTER(name, inp)                                                          \
+  mod->ph_module->with("py" #name "_" + inp, name##_to_py, concurrency::serial)                    \
+    .transform(inp)                                                                                \
+    .to("py" + inp)
+
+#define INSERT_OUTPUT_CONVERTER(name, outp)                                                        \
+  mod->ph_module->with(#name "py_" + outp, py_to_##name, concurrency::serial)                      \
+    .transform("py" + output)                                                                      \
+    .to(output)
+
 static PyObject* md_register(py_phlex_module* mod, PyObject* args, PyObject* /*kwds*/)
 {
   // Register a python algorithm by adding the necessary intermediate converter
@@ -139,6 +232,15 @@ static PyObject* md_register(py_phlex_module* mod, PyObject* args, PyObject* /*k
     PyErr_SetString(PyExc_TypeError, "provided algorithm is not callable");
     return nullptr;
   }
+
+  // retrieve function name and argument types
+  PyObject* pyname = PyObject_GetAttrString(callable, "__name__");
+  if (!pyname) {
+    // AttributeError already set
+    return nullptr;
+  }
+  std::string cname = PyUnicode_AsUTF8(pyname);
+  Py_DECREF(pyname);
 
   PyObject* inputs = PyTuple_GetItem(args, 1);
   PyObject* outputs = PyTuple_GetItem(args, 2);
@@ -156,25 +258,69 @@ static PyObject* md_register(py_phlex_module* mod, PyObject* args, PyObject* /*k
   }
   std::string const& output = coutputs[0];
 
-  // insert input converter nodes into the graph
-  for (auto const& inp : cinputs) {
-    mod->ph_module->with("pyint_" + inp, pyint, concurrency::serial).transform(inp).to("py" + inp);
+  std::vector<std::string> input_types;
+  input_types.reserve(cinputs.size());
+  std::string output_type; // TODO: accept a tuple (see also above)
+
+  PyObject* annot = PyObject_GetAttrString(callable, "__annotations__");
+  if (annot && PyDict_Check(annot) && 1 < PyDict_Size(annot)) {
+    PyObject* ret = PyDict_GetItemString(annot, "return");
+
+    if (ret) {
+      output_type = annotation_as_text(ret);
+
+      PyObject* values = PyDict_Values(annot);
+      for (Py_ssize_t i = 0; i < (PyList_GET_SIZE(values) - 1); ++i) {
+        PyObject* item = PyList_GET_ITEM(values, i);
+        input_types.emplace_back(annotation_as_text(item));
+      }
+      Py_DECREF(values);
+    }
+  }
+  Py_XDECREF(annot);
+
+  if (output_type.empty() or input_types.size() != cinputs.size()) {
+    PyErr_SetString(PyExc_TypeError, "annotions not found or unknown formatting");
+    return nullptr;
   }
 
-  // register Pythobn algorithm
+  // insert input converter nodes into the graph
+  for (size_t i = 0; i < (size_t)cinputs.size(); ++i) {
+    // TODO: this seems overly verbose and inefficient, but the function needs
+    // to be properly types, so every option is made explicit
+    auto const& inp = cinputs[i];
+    auto const& inp_type = input_types[i];
+
+    if (inp_type == "bool")
+      INSERT_INPUT_CONVERTER(bool, inp);
+    else if (inp_type == "int")
+      INSERT_INPUT_CONVERTER(int, inp);
+    else if (inp_type == "unsigned int")
+      INSERT_INPUT_CONVERTER(uint, inp);
+    else if (inp_type == "long")
+      INSERT_INPUT_CONVERTER(long, inp);
+    else if (inp_type == "unsigned long")
+      INSERT_INPUT_CONVERTER(ulong, inp);
+    else if (inp_type == "float")
+      INSERT_INPUT_CONVERTER(float, inp);
+    else if (inp_type == "double")
+      INSERT_INPUT_CONVERTER(double, inp);
+  }
+
+  // register Python algorithm
   if (cinputs.size() == 1) {
     auto* pyc = new py_callback_1{callable}; // TODO: leaks, but has program lifetime
-    mod->ph_module->with("add", *pyc, concurrency::serial)
+    mod->ph_module->with(cname, *pyc, concurrency::serial)
       .transform("py" + cinputs[0])
       .to("py" + output);
   } else if (cinputs.size() == 2) {
     auto* pyc = new py_callback_2{callable}; // TODO: id.
-    mod->ph_module->with("add", *pyc, concurrency::serial)
+    mod->ph_module->with(cname, *pyc, concurrency::serial)
       .transform("py" + cinputs[0], "py" + cinputs[1])
       .to("py" + output);
   } else if (cinputs.size() == 3) {
     auto* pyc = new py_callback_3{callable}; // TODO: id
-    mod->ph_module->with("add", *pyc, concurrency::serial)
+    mod->ph_module->with(cname, *pyc, concurrency::serial)
       .transform("py" + cinputs[0], "py" + cinputs[1], "py" + cinputs[2])
       .to("py" + output);
   } else {
@@ -182,10 +328,22 @@ static PyObject* md_register(py_phlex_module* mod, PyObject* args, PyObject* /*k
     return nullptr;
   }
 
-  // insert output converter node into the graph
-  mod->ph_module->with("intpy_" + output, intpy, concurrency::serial)
-    .transform("py" + output)
-    .to(output);
+  // insert output converter node into the graph (TODO: same as above; these
+  // are explicit b/c of the templates only)
+  if (output_type == "bool")
+    INSERT_OUTPUT_CONVERTER(bool, output);
+  else if (output_type == "int")
+    INSERT_OUTPUT_CONVERTER(int, output);
+  else if (output_type == "unsigned int")
+    INSERT_OUTPUT_CONVERTER(uint, output);
+  else if (output_type == "long")
+    INSERT_OUTPUT_CONVERTER(long, output);
+  else if (output_type == "unsigned long")
+    INSERT_OUTPUT_CONVERTER(ulong, output);
+  else if (output_type == "float")
+    INSERT_OUTPUT_CONVERTER(float, output);
+  else if (output_type == "double")
+    INSERT_OUTPUT_CONVERTER(double, output);
 
   Py_RETURN_NONE;
 }
