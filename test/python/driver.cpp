7ff5946fdef6890f285dc6486133cf7c65afa104
Wim Lavrijsen <WLavrijsen@lbl.gov>
2025-11-03 15:47:01 -0800

    make sure that the adder sum result is non-zero

--- test/python/driver.cpp
+++ test/python/driver.cpp
@@ -19,7 +19,7 @@ namespace {
       for (int i : std::views::iota(1, n_ + 1)) {
         auto store = job_store->make_child(i, "event");
         store->add_product("i", i);
-        store->add_product("j", -i);
+        store->add_product("j", -i + 1);
         driver.yield(store);
       }
     }
