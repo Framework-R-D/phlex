f2c81975d4cb3cfd3a2e9e0f08f9c7f865f8b1f4
Chris Green <greenc@fnal.gov>
2025-11-20 10:16:10 -0600

    Fix CMake format

--- Modules/private/EnableGccAnalyzer.cmake
+++ Modules/private/EnableGccAnalyzer.cmake
@@ -1,12 +1,14 @@
 # --- EnableGccAnalyzer.cmake
 #
-# This module checks if the ENABLE_GCC_ANALYZER option is enabled, and if so,
-# it adds the necessary compiler flags to enable the GCC static analyzer.
+# This module checks if the ENABLE_GCC_ANALYZER option is enabled, and if so, it
+# adds the necessary compiler flags to enable the GCC static analyzer.
 
 if(ENABLE_GCC_ANALYZER)
   # Check if the compiler is GNU
   if(NOT CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    message(FATAL_ERROR "GCC Static Analyzer is only supported with the GNU compiler.")
+    message(
+      FATAL_ERROR "GCC Static Analyzer is only supported with the GNU compiler."
+      )
   else()
     message(STATUS "Enabling GCC Static Analyzer")
     add_compile_options(-fanalyzer)
