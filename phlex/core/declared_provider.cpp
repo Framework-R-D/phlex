eb8bfab9ac2e21fa944872dd5da8de89820c100b
Kyle Knoepfel <knoepfel@fnal.gov>
2025-12-10 14:58:09 -0600

    Simplify declared_provider class

--- phlex/core/declared_provider.cpp
+++ phlex/core/declared_provider.cpp
@@ -1,12 +1,19 @@
 #include "phlex/core/declared_provider.hpp"
 
 namespace phlex::experimental {
-  declared_provider::declared_provider(algorithm_name name, product_queries output_products) :
-    products_consumer{std::move(name), {}, std::move(output_products)}
+  declared_provider::declared_provider(algorithm_name name, product_query output_product) :
+    name_{std::move(name)}, output_product_{std::move(output_product)}
   {
   }
 
   declared_provider::~declared_provider() = default;
+
+  std::string declared_provider::full_name() const { return name_.full(); }
+
+  product_query const& declared_provider::output_product() const noexcept
+  {
+    return output_product_;
+  }
 
   void declared_provider::report_cached_stores(stores_t const& stores) const
   {
