d71addc86c6a5c41a0dc783ef2bea0d83abdc1ef
Marc Paterno <paterno@fnal.gov>
2025-12-16 10:35:01 -0600

    Modify unfold_transform_fold to use generator

--- test/demo-giantdata/unfold_transform_fold.cpp
+++ test/demo-giantdata/unfold_transform_fold.cpp
@@ -2,6 +2,7 @@
 #include "phlex/model/data_cell_index.hpp"
 #include "phlex/model/product_store.hpp"
 #include "phlex/utilities/async_driver.hpp"
+#include "plugins/layer_generator.hpp"
 #include "test/products_for_output.hpp"
 
 #include "test/demo-giantdata/user_algorithms.hpp"
@@ -9,7 +10,6 @@
 #include "test/demo-giantdata/waveform_generator_input.hpp"
 
 #include <algorithm>
-#include <ranges>
 #include <string>
 #include <vector>
 
@@ -51,37 +51,17 @@ int main(int argc, char* argv[])
 
   std::size_t const wires_per_spill = apas_per_spill * 256ull;
 
-  // Create some data layers of the data-layer hierarchy.
-  // We may or may not want to create pre-generated data layers.
-  // Each data layer gets an index number in the hierarchy.
-
-  auto source = [n_runs, n_subruns, n_spills](framework_driver& driver) {
-    auto job_index = data_cell_index::base_ptr();
-    driver.yield(job_index);
-
-    // job -> run -> subrun -> spill data layers
-    for (unsigned runno : std::views::iota(0u, n_runs)) {
-      auto run_index = job_index->make_child(runno, "run");
-      driver.yield(run_index);
-
-      for (unsigned subrunno : std::views::iota(0u, n_subruns)) {
-        auto subrun_index = run_index->make_child(subrunno, "subrun");
-        driver.yield(subrun_index);
-
-        for (unsigned spillno : std::views::iota(0u, n_spills)) {
-
-          auto spill_index = subrun_index->make_child(spillno, "spill");
-          driver.yield(spill_index);
-        }
-      }
-    }
-  };
+  // Create data layers using layer generator
+  layer_generator gen;
+  gen.add_layer("run", {"job", n_runs});
+  gen.add_layer("subrun", {"run", n_subruns});
+  gen.add_layer("spill", {"subrun", n_spills});
 
   // Create the graph. The source tells us what data we will process.
   // We introduce a new scope to make sure the graph is destroyed before we
   // write out the logged records.
   {
-    framework_graph g{source};
+    framework_graph g{driver_for_test(gen)};
 
     // Add the unfold node to the graph. We do not yet know how to provide the chunksize
     // to the constructor of the WaveformGenerator, so we will use the default value.
