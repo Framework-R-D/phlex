e3b18f6bd83e63c6681774369b10429be9de3186
Wim Lavrijsen <WLavrijsen@lbl.gov>
2025-12-16 23:08:25 -0800

    upgrade to new registration interface (note: DOES NOT WORK)

--- test/python/driver.cpp
+++ test/python/driver.cpp
@@ -1,44 +1,11 @@
-#include "phlex/model/product_store.hpp"
-#include "phlex/source.hpp"
+#include "phlex/module.hpp"
 
-#include <memory>
-#include <numeric>
-#include <ranges>
-#include <vector>
+using namespace phlex::experimental;
 
-namespace {
-  class number_generator {
-  public:
-    number_generator(phlex::experimental::configuration const& config) :
-      n_{config.get<int>("max_numbers")}, coll_{config.get<bool>("as_collection")}
-    {
-    }
-
-    void next(phlex::experimental::framework_driver& driver) const
-    {
-      auto job_store = phlex::experimental::product_store::base();
-      driver.yield(job_store);
-
-      if (coll_) {
-        auto pv = std::make_shared<std::vector<int>>(n_);
-        std::iota(pv->begin(), pv->end(), 1);
-        auto store = job_store->make_child(0, "event");
-        store->add_product("coll_of_i", pv);
-        driver.yield(store);
-      } else {
-        for (int i : std::views::iota(1, n_ + 1)) {
-          auto store = job_store->make_child(i, "event");
-          store->add_product("i", i);
-          store->add_product("j", -i + 1);
-          driver.yield(store);
-        }
-      }
-    }
-
-  private:
-    int n_;
-    bool coll_;
-  };
+PHLEX_EXPERIMENTAL_REGISTER_ALGORITHMS(m)
+{
+  m.provide("provide_i", [](data_cell_index const& id) -> int { return id.number(); })
+    .output_product(product_query{product_specification::create("i")});
+  m.provide("provide_j", [](data_cell_index const& id) -> int { return -id.number() + 1; })
+    .output_product(product_query{product_specification::create("i")});
 }
-
-PHLEX_EXPERIMENTAL_REGISTER_SOURCE(number_generator)
