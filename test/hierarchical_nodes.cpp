1df9a750243c2b0ede8ddd55be149103506d047f
Kyle Knoepfel <knoepfel@fnal.gov>
2026-01-21 16:55:35 -0600

    Use strict equality comparisons

--- test/hierarchical_nodes.cpp
+++ test/hierarchical_nodes.cpp
@@ -134,7 +134,7 @@ TEST_CASE("Hierarchical nodes", "[graph]")
 
   CHECK(g.execution_count("square") == index_limit * number_limit);
   CHECK(g.execution_count("add") == index_limit * number_limit);
-  CHECK(g.execution_count("get_the_time") >= index_limit);
+  CHECK(g.execution_count("get_the_time") == index_limit);
   CHECK(g.execution_count("scale") == index_limit);
   CHECK(g.execution_count("print_result") == index_limit);
 }
