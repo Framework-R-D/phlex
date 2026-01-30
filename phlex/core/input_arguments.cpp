99750c2e9cfff7d17074358202461c4266332493
Beojan Stanislaus <bstanislaus@lbl.gov>
2026-01-29 16:03:31 -0800

    Undo changes to input_arguments.cpp

--- phlex/core/input_arguments.cpp
+++ phlex/core/input_arguments.cpp
@@ -2,22 +2,32 @@
 
 #include "fmt/format.h"
 
+#include <algorithm>
+#include <set>
 #include <stdexcept>
+#include <vector>
 
 namespace phlex::experimental::detail {
   void verify_no_duplicate_input_products(std::string const& algorithm_name,
-                                          product_queries input_queries)
+                                          product_queries to_sort)
   {
-    for (std::size_t i = 0; i < input_queries.size(); ++i) {
-      for (std::size_t j = i + 1; j < input_queries.size(); ++j) {
-        if (input_queries[i].match(input_queries[j]) || input_queries[j].match(input_queries[i])) {
-          throw std::runtime_error(
-            fmt::format("The algorithm {} has at least one duplicate input:\n- {}\n- {}\n",
-                        algorithm_name,
-                        input_queries[i].to_string(),
-                        input_queries[j].to_string()));
-        }
-      }
+    std::sort(begin(to_sort), end(to_sort));
+    std::set unique_and_sorted(begin(to_sort), end(to_sort));
+    product_queries duplicates;
+    std::set_difference(begin(to_sort),
+                        end(to_sort),
+                        begin(unique_and_sorted),
+                        end(unique_and_sorted),
+                        std::back_inserter(duplicates));
+    if (empty(duplicates)) {
+      return;
     }
+
+    std::string error =
+      fmt::format("Algorithm '{}' uses the following products more than once:\n", algorithm_name);
+    for (auto const& label : duplicates) {
+      error += fmt::format(" - '{}'\n", label.to_string());
+    }
+    throw std::runtime_error(error);
   }
 }
