7ade57d287005530b67e6ee6d8633906b9d36285
Kyle Knoepfel <knoepfel@fnal.gov>
2026-02-03 13:40:12 -0600

    Some refactoring

--- test/repeater/repeater_node.hpp
+++ test/repeater/repeater_node.hpp
@@ -23,143 +23,41 @@ namespace phlex::test {
   class repeater_node :
     public tbb::flow::composite_node<repeater_node_input<T>, indexed_message_tuple<T>> {
     using base_t = tbb::flow::composite_node<repeater_node_input<T>, indexed_message_tuple<T>>;
-    using tagged_repeater_msg =
+    using tagged_msg_t =
       tbb::flow::tagged_msg<std::size_t, indexed_message<T>, indexed_end_token, index_message>;
+    using multifunction_node_t =
+      tbb::flow::multifunction_node<tagged_msg_t, indexed_message_tuple<T>>;
 
     struct cached_product {
-      std::shared_ptr<indexed_message<T>> msg_with_product;
+      std::shared_ptr<indexed_message<T>> msg;
       tbb::concurrent_queue<std::size_t> msg_ids{};
       std::atomic<int> counter;
       std::atomic_flag flush_received{};
     };
 
-    using cached_products_t = tbb::concurrent_hash_map<std::size_t, cached_product>;
-    using accessor = cached_products_t::accessor;
-    using const_accessor = cached_products_t::const_accessor;
+    using cache_t = tbb::concurrent_hash_map<std::size_t, cached_product>;
+    using accessor = cache_t::accessor;
+    using const_accessor = cache_t::const_accessor;
 
   public:
     repeater_node(tbb::flow::graph& g) :
       base_t{g},
       indexer_{g},
-      repeater_{
-        g, tbb::flow::unlimited, [this](tagged_repeater_msg const& tagged, auto& outputs) {
-          // spdlog::warn("[{}/{}] Received {}", node_name_, layer_, tagged.tag());
-          cached_product* entry{nullptr};
-          data_cell_index_ptr current_index{};
-          std::size_t key = -1ull;
-          auto& output = std::get<0>(outputs);
+      repeater_{g, tbb::flow::unlimited, [this](tagged_msg_t const& tagged, auto& outputs) {
+                  auto& output = std::get<0>(outputs);
 
-          auto drain_cache = [&output](data_cell_index_ptr const& index,
-                                       cached_product* entry,
-                                       std::string const& node_name [[maybe_unused]],
-                                       std::string const& layer [[maybe_unused]]) -> int {
-            assert(entry->msg_with_product);
-            int counter{};
-            std::size_t new_msg_id{};
-            while (entry->msg_ids.try_pop(new_msg_id)) {
-              // spdlog::info("[{}/{}] => Outputting {} ({})",
-              //              node_name,
-              //              layer,
-              //              index->to_string(),
-              //              index->hash());
-              output.try_put(
-                {.msg_id = new_msg_id, .index = index, .data = entry->msg_with_product->data});
-              ++counter;
-            }
-            return counter;
-          };
+                  std::size_t key = -1ull;
+                  if (tagged.template is_a<indexed_message<T>>()) {
+                    key =
+                      handle_data_message(tagged.template cast_to<indexed_message<T>>(), output);
+                  } else if (tagged.template is_a<indexed_end_token>()) {
+                    key = handle_flush_token(tagged.template cast_to<indexed_end_token>());
+                  } else {
+                    key = handle_index_message(tagged.template cast_to<index_message>(), output);
+                  }
 
-          if (tagged.template is_a<indexed_message<T>>()) {
-            auto const& msg = tagged.template cast_to<indexed_message<T>>();
-            current_index = msg.index;
-            key = msg.index->hash();
-            if (!caching_required_) {
-              spdlog::debug(
-                "[{}/{}] -> Outputting {} ({})", node_name_, layer_, msg.index->to_string(), key);
-              output.try_put(msg);
-            } else {
-              assert(msg.data);
-              accessor a;
-              cached_products_.insert(a, key);
-              entry = &a->second;
-              spdlog::debug("[{}/{}] Caching product for {} ({})",
-                            node_name_,
-                            layer_,
-                            msg.index->to_string(),
-                            key);
-              entry->msg_with_product = std::make_shared<indexed_message<T>>(msg);
-              entry->counter += drain_cache(msg.index, entry, node_name_, layer_);
-            }
-          } else if (tagged.template is_a<indexed_end_token>()) {
-            auto const [index, count] = tagged.template cast_to<indexed_end_token>();
-            spdlog::debug(
-              "[{}/{}] Received flush {} ({})", node_name_, layer_, count, index->to_string());
-            current_index = index;
-            key = index->hash();
-            accessor a;
-            cached_products_.insert(a, key);
-            entry = &a->second;
-            entry->counter -= count;
-            std::ignore = entry->flush_received.test_and_set();
-          } else {
-
-            auto const [msg_id, index, cache] = tagged.template cast_to<index_message>();
-            key = index->hash();
-
-            if (caching_required_ and !cache) {
-              caching_required_ = false;
-              // No caching needed
-              accessor a;
-              if (cached_products_.find(a, key)) {
-                entry = &a->second;
-                if (entry->msg_with_product) {
-                  spdlog::debug(
-                    "[{}/{}] +> Outputting {} ({})", node_name_, layer_, index->to_string(), key);
-                  output.try_put(*entry->msg_with_product);
-                  ++entry->counter;
-                }
-              }
-            } else if (caching_required_) {
-              // Caching required
-              current_index = index;
-              key = index->hash();
-              accessor a;
-              cached_products_.insert(a, key);
-              entry = &a->second;
-              if (entry->msg_with_product) {
-                spdlog::debug(
-                  "[{}/{}] <> Outputting {} ({})", node_name_, layer_, index->to_string(), key);
-                output.try_put(
-                  {.msg_id = msg_id, .index = index, .data = entry->msg_with_product->data});
-                entry->counter += 1 + drain_cache(index, entry, node_name_, layer_);
-              } else {
-                entry->msg_ids.push(msg_id);
-              }
-            }
-          }
-
-          // Cleanup
-          if (accessor a; cached_products_.find(a, key)) {
-            entry = &a->second;
-            if (!caching_required_) {
-              if (entry->counter == 0) {
-                spdlog::debug("[{}/{}] ++ Outputting {} ({})",
-                              node_name_,
-                              layer_,
-                              entry->msg_with_product->index->to_string(),
-                              key);
-                output.try_put(*entry->msg_with_product);
-                ++entry->counter;
-              }
-              assert(entry->counter == 1);
-              cached_products_.erase(a);
-            } else if (entry->flush_received.test() and entry->counter == 0) {
-              spdlog::trace("Erasing cached product {}", key);
-              cached_products_.erase(a);
-            }
-            entry = nullptr; // Do not attempt to use entry (stale!)
-          }
-        }}
+                  cleanup_cache_entry(key, output);
+                }}
     {
       base_t::set_external_ports(typename base_t::input_ports_type{input_port<0>(indexer_),
                                                                    input_port<1>(indexer_),
@@ -177,13 +75,12 @@ namespace phlex::test {
       if (cached_products_.empty()) {
         return;
       }
+
       spdlog::warn("[{}/{}] Cached products {}", node_name_, layer_, cached_products_.size());
       for (auto const& [_, cache] : cached_products_) {
-        if (cache.msg_with_product) {
-          spdlog::warn("[{}/{}]   Product for {}",
-                       node_name_,
-                       layer_,
-                       cache.msg_with_product->index->to_string());
+        if (cache.msg) {
+          spdlog::warn(
+            "[{}/{}]   Product for {}", node_name_, layer_, cache.msg->index->to_string());
         } else {
           spdlog::warn("[{}/{}]   Product for {}", node_name_, layer_, _);
         }
@@ -197,12 +94,114 @@ namespace phlex::test {
     }
 
   private:
+    using output_port_t = std::tuple_element_t<0, typename multifunction_node_t::output_ports_type>;
+
+    int emit_pending_ids(data_cell_index_ptr const& index,
+                         cached_product* entry,
+                         output_port_t& output)
+    {
+      assert(entry->msg);
+      int num_emitted{};
+      std::size_t msg_id{};
+      while (entry->msg_ids.try_pop(msg_id)) {
+        output.try_put({.msg_id = msg_id, .index = index, .data = entry->msg->data});
+        ++num_emitted;
+      }
+      return num_emitted;
+    }
+
+    std::size_t handle_data_message(indexed_message<T> const& msg, output_port_t& output)
+    {
+      auto const key = msg.index->hash();
+
+      // Pass-through mode; output directly without caching
+      if (!cache_enabled_) {
+        output.try_put(msg);
+        return key;
+      }
+
+      // Caching mode; store product and drain any pending message IDs
+      assert(msg.data);
+      accessor a;
+      cached_products_.insert(a, key);
+      auto* entry = &a->second;
+      entry->msg = std::make_shared<indexed_message<T>>(msg);
+      entry->counter += emit_pending_ids(msg.index, entry, output);
+      return key;
+    }
+
+    std::size_t handle_flush_token(indexed_end_token const& token)
+    {
+      auto const& [index, count] = token;
+      auto const key = index->hash();
+      accessor a;
+      cached_products_.insert(a, key);
+      auto* entry = &a->second;
+      entry->counter -= count;
+      std::ignore = entry->flush_received.test_and_set();
+      return key;
+    }
+
+    std::size_t handle_index_message(index_message const& msg, output_port_t& output)
+    {
+      auto const& [msg_id, index, cache] = msg;
+      auto const key = index->hash();
+
+      // Caching already disabled; no action needed
+      if (!cache_enabled_) {
+        return key;
+      }
+
+      // Transition to pass-through mode; output any cached product and disable caching
+      if (!cache) {
+        cache_enabled_ = false;
+        if (accessor a; cached_products_.find(a, key)) {
+          auto* entry = &a->second;
+          if (entry->msg) {
+            output.try_put(*entry->msg);
+            ++entry->counter;
+          }
+        }
+        return key;
+      }
+
+      // Normal caching mode; either output cached product or queue message ID for later
+      accessor a;
+      cached_products_.insert(a, key);
+      auto* entry = &a->second;
+      if (entry->msg) {
+        output.try_put({.msg_id = msg_id, .index = index, .data = entry->msg->data});
+        entry->counter += 1 + emit_pending_ids(index, entry, output);
+      } else {
+        entry->msg_ids.push(msg_id);
+      }
+      return key;
+    }
+
+    void cleanup_cache_entry(std::size_t key, output_port_t& output)
+    {
+      accessor a;
+      if (!cached_products_.find(a, key)) {
+        return;
+      }
+
+      auto* entry = &a->second;
+      if (!cache_enabled_) {
+        if (entry->counter == 0) {
+          output.try_put(*entry->msg);
+          ++entry->counter;
+        }
+        assert(entry->counter == 1);
+        cached_products_.erase(a);
+      } else if (entry->flush_received.test() and entry->counter == 0) {
+        cached_products_.erase(a);
+      }
+    }
+
     tbb::flow::indexer_node<indexed_message<T>, indexed_end_token, index_message> indexer_;
-    tbb::flow::multifunction_node<tagged_repeater_msg, indexed_message_tuple<T>> repeater_;
+    multifunction_node_t repeater_;
     tbb::concurrent_hash_map<std::size_t, cached_product> cached_products_; // Key is the index hash
-    std::atomic<unsigned> n_cached_products_{};
-    std::atomic<bool> caching_required_{true};
-    std::atomic<bool> erase_cache_{false};
+    std::atomic<bool> cache_enabled_{true};
     std::string node_name_;
     std::string layer_;
   };
