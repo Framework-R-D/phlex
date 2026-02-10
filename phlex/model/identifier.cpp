0d126ed326fefeab5c696c6f3dc5eff1d3072858
Beojan Stanislaus <bstanislaus@lbl.gov>
2026-02-10 10:22:49 +0100

    Compare strings as well in identifer

--- phlex/model/identifier.cpp
+++ phlex/model/identifier.cpp
@@ -21,10 +21,20 @@ namespace phlex::experimental {
   identifier::identifier(std::string_view str) : content_(str), hash_(hash_string(content_)) {}
 
   identifier::operator std::string_view() const noexcept { return std::string_view(content_); }
-  bool identifier::operator==(identifier const& rhs) const noexcept { return hash_ == rhs.hash_; }
+  bool identifier::operator==(identifier const& rhs) const noexcept
+  {
+    if (hash_ == rhs.hash_) {
+      return content_ == rhs.content_;
+    }
+    return false;
+  }
   std::strong_ordering identifier::operator<=>(identifier const& rhs) const noexcept
   {
-    return hash_ <=> rhs.hash_;
+    std::strong_ordering hash_cmp = hash_ <=> rhs.hash_;
+    if (hash_cmp == 0) {
+      return content_ <=> rhs.content_;
+    }
+    return hash_cmp;
   }
 
   bool operator==(identifier const& lhs, identifier_query rhs) { return lhs.hash_ == rhs.hash; }
