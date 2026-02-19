4223a41f68749cd578712c2983da4a7732ed6b9f
Beojan Stanislaus <bstanislaus@lbl.gov>
2026-02-19 14:38:51 -0800

    Make algorithm_name use identifiers

--- phlex/model/identifier.hpp
+++ phlex/model/identifier.hpp
@@ -21,6 +21,9 @@ namespace phlex::experimental {
   class identifier {
   public:
     static std::uint64_t hash_string(std::string_view str);
+    // The default constructor turns out to be necessary so other classes containing identifiers
+    // can have default constructors. For now, identifier() != ""_id. Let's see how it goes.
+    identifier() = default;
     identifier(identifier const& other) = default;
     identifier(identifier&& other) noexcept = default;
 
@@ -50,7 +53,7 @@ namespace phlex::experimental {
 
   private:
     std::string content_;
-    std::uint64_t hash_;
+    std::uint64_t hash_{0};
   };
 
   // Identifier UDL
