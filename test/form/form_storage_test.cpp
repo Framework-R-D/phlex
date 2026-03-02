0435987cb17975edc257577a9d65cd58c42de0b6
aolivier23 <36601053+aolivier23@users.noreply.github.com>
2026-03-02 10:31:32 -0600

    FORM Storage Tests (#354)
    * Add a unit test requiring that FORM throws an exception when trying to read a data product from disk with a type that doesn't match the type on disk.
    * Added test that two FORM AssociativeContainers can share the same Association.
    * Added FORM test that three Associative_Containers can share the same Association.  
      - This models a physics algorithm in phlex writing two data products through FORM.
    * Added FORM tests for error handling when setting up Containers.
    * Made form::test::read<>() return std::unique_ptr<> instead of copies of dangling pointers.
    * Updated (so-far-unused) form::test::getTechnology() to use form::technology::ROOT_TTREE for default instead of raw integers for better maintainability.
    * Made all test_utils.hpp inline in case someone writes a test complicated enough that it gets included twice.
    * Moved FORM's USE_ROOT_STORAGE compile definition to somewhere where it propagates automatically: the root_storage library itself. 
    * Updated include guards in test_utils.hpp to match new convention on main.
    * Removed unneeded assert()s from FORM Storage test.
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ test/form/form_storage_test.cpp
@@ -0,0 +1,109 @@
+//Tests for FORM's storage layer's design requirements
+
+#include "test/form/test_utils.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <cmath>
+#include <numeric>
+#include <vector>
+
+using namespace form::detail::experimental;
+
+TEST_CASE("Storage_Container read wrong type", "[form]")
+{
+  int const technology = form::technology::ROOT_TTREE;
+  std::vector<int> primes = {2, 3, 5, 7, 11, 13, 17, 19};
+  form::test::write(technology, primes);
+
+  auto file = createFile(technology, form::test::testFileName, 'i');
+  auto container = createContainer(technology, form::test::makeTestBranchName<std::vector<int>>());
+  container->setFile(file);
+  void const* dataPtr;
+  CHECK_THROWS_AS(container->read(0, &dataPtr, typeid(double)), std::runtime_error);
+}
+
+TEST_CASE("Storage_Container sharing an Association", "[form]")
+{
+  int const technology = form::technology::ROOT_TTREE;
+  std::vector<float> piData(10, 3.1415927);
+  std::string indexData = "[EVENT=00000001;SEG=00000001]";
+
+  form::test::write(technology, piData, indexData);
+
+  auto [piResult, indexResult] = form::test::read<std::vector<float>, std::string>(technology);
+
+  float const originalSum = std::accumulate(piData.begin(), piData.end(), 0.f);
+  float const readSum = std::accumulate(piResult->begin(), piResult->end(), 0.f);
+  float const floatDiff = readSum - originalSum;
+
+  SECTION("float container sum") { CHECK(fabs(floatDiff) < std::numeric_limits<float>::epsilon()); }
+
+  SECTION("index") { CHECK(*indexResult == indexData); }
+}
+
+TEST_CASE("Storage_Container multiple containers in Association", "[form]")
+{
+  int const technology = form::technology::ROOT_TTREE;
+  std::vector<float> piData(10, 3.1415927);
+  std::vector<int> magicData(17);
+  std::iota(magicData.begin(), magicData.end(), 42);
+  std::string indexData = "[EVENT=00000001;SEG=00000001]";
+
+  form::test::write(technology, piData, magicData, indexData);
+
+  auto [piResult, magicResult, indexResult] =
+    form::test::read<std::vector<float>, std::vector<int>, std::string>(technology);
+
+  SECTION("float container")
+  {
+    float const originalSum = std::accumulate(piData.begin(), piData.end(), 0.f);
+    float const readSum = std::accumulate(piResult->begin(), piResult->end(), 0.f);
+    float const floatDiff = readSum - originalSum;
+    CHECK(fabs(floatDiff) < std::numeric_limits<float>::epsilon());
+  }
+
+  SECTION("int container")
+  {
+    int const originalMagic = std::accumulate(magicData.begin(), magicData.end(), 0);
+    int const readMagic = std::accumulate(magicResult->begin(), magicResult->end(), 0);
+    int const magicDiff = readMagic - originalMagic;
+    CHECK(magicDiff == 0);
+  }
+
+  SECTION("index data") { CHECK(*indexResult == indexData); }
+}
+
+TEST_CASE("FORM Container setup error handling")
+{
+  int const technology = form::technology::ROOT_TTREE;
+  auto file = createFile(technology, "testContainerErrorHandling.root", 'o');
+  auto container = createContainer(technology, "test/testData");
+
+  std::vector<float> testData;
+  void const* ptrTestData = &testData;
+  auto const& typeInfo = typeid(testData);
+
+  SECTION("fill() before setParent()")
+  {
+    CHECK_THROWS_AS(container->setupWrite(typeInfo), std::runtime_error);
+    CHECK_THROWS_AS(container->fill(ptrTestData), std::runtime_error);
+  }
+
+  SECTION("commit() before setParent()")
+  {
+    CHECK_THROWS_AS(container->commit(), std::runtime_error);
+  }
+
+  SECTION("read() before setParent()")
+  {
+    CHECK_THROWS_AS(container->read(0, &ptrTestData, typeInfo), std::runtime_error);
+  }
+
+  SECTION("mismatched file type")
+  {
+    std::shared_ptr<IStorage_File> wrongFile(
+      new Storage_File("testContainerErrorHandling.root", 'o'));
+    CHECK_THROWS_AS(container->setFile(wrongFile), std::runtime_error);
+  }
+}
