9bc8f2e9ccd04282de9ce45f8771530f2f24440d
Chris Green <greenc@fnal.gov>
2026-04-21 10:17:13 -0500

    Resolve emergent style issues from #525 after rebase from `main`

--- test/python/test_mismatch.py
+++ test/python/test_mismatch.py
@@ -14,13 +14,15 @@ def PHLEX_REGISTER_ALGORITHMS(m, config):
     """Register algorithms."""
     # test a range of possible errors in the annotation and product specifications
     funky1 = List[int] | Dict[int, int]
-    funky2 = b'\x80'.decode('gbk', errors='surrogateescape')
-    funky3 = "" # empty string
+    funky2 = b"\x80".decode("gbk", errors="surrogateescape")
+    funky3 = ""  # empty string
 
     test_count = 0
-    for funky, exc, err in ((funky1, TypeError, "must be unambiguous"),
-                            (funky2, UnicodeEncodeError, "surrogates not allowed"),
-                            (funky3, TypeError, "unsupported"),):
+    for funky, exc, err in (
+        (funky1, TypeError, "must be unambiguous"),
+        (funky2, UnicodeEncodeError, "surrogates not allowed"),
+        (funky3, TypeError, "unsupported"),
+    ):
         funky_input1 = Variant(mismatch_func, {"a": funky, "b": int, "return": int}, "finp1")
         funky_input2 = Variant(mismatch_func, {"a": int, "b": funky, "return": int}, "finp2")
         funky_return = Variant(mismatch_func, {"a": int, "b": int, "return": funky}, "fret")
@@ -29,8 +31,10 @@ def PHLEX_REGISTER_ALGORITHMS(m, config):
             try:
                 m.transform(
                     f,
-                    input_family=[{"creator": "input", "layer": "event", "suffix": "i"},
-                                  {"creator": "input", "layer": "event", "suffix": "j"}],
+                    input_family=[
+                        {"creator": "input", "layer": "event", "suffix": "i"},
+                        {"creator": "input", "layer": "event", "suffix": "j"},
+                    ],
                     output_product_suffixes=["sum"],
                 )
                 assert not "supposed to be here"
@@ -45,8 +49,10 @@ def PHLEX_REGISTER_ALGORITHMS(m, config):
     try:
         m.transform(
             funky_func,
-            input_family=[{"creator": "input", "layer": "event", "suffix": "i"},
-                          {"creator": "input", "layer": "event", "suffix": "j"}],
+            input_family=[
+                {"creator": "input", "layer": "event", "suffix": "i"},
+                {"creator": "input", "layer": "event", "suffix": "j"},
+            ],
             output_product_suffixes=["sum"],
         )
         assert not "supposed to be here"
