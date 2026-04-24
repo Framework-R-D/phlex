ded1d21cdb8f7bf82b7af689ece66f6af26dd1a7
Chris Green <greenc@fnal.gov>
2026-04-24 10:35:11 -0500

    Use `unique_ptr` with a custom deleter for clarity and consistency
    Be absolutely clear about the fact that we are managing the TTree's
    memory ourselves to ensure that it is deleted at the right time, and
    _after_ we ensure that pending objects have been written to the tree.

--- form/root_storage/root_ttree_write_container.hpp
+++ form/root_storage/root_ttree_write_container.hpp
@@ -16,7 +16,6 @@ namespace form::detail::experimental {
   class ROOT_TTree_Write_ContainerImp : public Storage_Write_Association {
   public:
     ROOT_TTree_Write_ContainerImp(std::string const& name);
-    ~ROOT_TTree_Write_ContainerImp() override;
 
     ROOT_TTree_Write_ContainerImp(ROOT_TTree_Write_ContainerImp const&) = delete;
     ROOT_TTree_Write_ContainerImp& operator=(ROOT_TTree_Write_ContainerImp const&) = delete;
@@ -31,8 +30,16 @@ namespace form::detail::experimental {
     TTree* getTTree();
 
   private:
+    // Be absolutely explicit about the ownership semantics of TTree*,
+    // since ROOT nominally manages the memory of TTree objects, but in
+    // practice we need to ensure objects are written to the TTree, and
+    //  the TTree itself is deleted at the right time.
+    struct TTreeDeleter {
+      void operator()(TTree* t) const;
+    };
+
     std::shared_ptr<TFile> m_tfile;
-    TTree* m_tree{nullptr};
+    std::unique_ptr<TTree, TTreeDeleter> m_tree;
   };
 
 } //namespace form::detail::experimental
