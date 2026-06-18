22e58323c276f6a289fe9103f64268a3b242bb55
Kyle Knoepfel <knoepfel@fnal.gov>
2026-06-18 09:59:13 -0500

    Fix sendable concept and sendable_type

--- phlex/core/fold/send.hpp
+++ phlex/core/fold/send.hpp
@@ -9,6 +9,9 @@
 //
 // For non-moveable types, an overload of 'T send(U const&)' is required, where U is the
 // unmoveable type, and T is the type that can be a represented as a data product.
+//
+// If a type both has a 'send(...)' overload and is move-constructible, the 'send(...)'
+// overload takes precedence.
 // =======================================================================================
 
 #include <atomic>
@@ -22,23 +25,30 @@ namespace phlex::experimental {
   }
 
   template <typename T>
-  struct sendable_type_impl {};
-
-  template <std::move_constructible T>
-  struct sendable_type_impl<T> {
-    using type = T;
+  concept has_send = requires(T const& t) {
+    { send(t) } -> std::move_constructible;
   };
 
   template <typename T>
-    requires requires(T const& t) {
-      { send(t) } -> std::move_constructible;
-    }
-  struct sendable_type_impl<T> {
-    using type = decltype(send(std::declval<T>()));
-  };
+  concept move_constructible_only = std::move_constructible<T> && !has_send<T>;
+
+  namespace detail {
+    template <typename T>
+    struct sendable_type_impl {};
+
+    template <has_send T>
+    struct sendable_type_impl<T> {
+      using type = decltype(send(std::declval<T const&>()));
+    };
+
+    template <move_constructible_only T>
+    struct sendable_type_impl<T> {
+      using type = T;
+    };
+  }
 
   template <typename T>
-  using sendable_type = typename sendable_type_impl<T>::type;
+  using sendable_type = typename detail::sendable_type_impl<T>::type;
 }
 
 #endif // PHLEX_CORE_FOLD_SEND_HPP
