a0fb46852639f237bcccd3ff800d11cdb1db560f
Wim Lavrijsen <WLavrijsen@lbl.gov>
2026-06-26 13:57:17 -0700

    guarantee that a path that is supposed to fail actually does

--- test/python/adder.py
+++ test/python/adder.py
@@ -61,6 +61,10 @@ def PHLEX_REGISTER_ALGORITHMS(m, config):
       m.transform(int_adder, input_family=config["input"])
     except TypeError as e:
       assert "should have an output suffix" in str(e)
+    else:
+      raise AssertionError(
+          "m.transform() should reject registrations without an output suffix"
+      )
 
     # functional transform registration
     m.transform(int_adder, input_family=config["input"], output_product_suffixes=config["output"])
