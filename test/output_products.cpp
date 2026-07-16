2d4d6f66aae983026eb5886d1f0ef447d7ba3e2d
Kyle Knoepfel <knoepfel@fnal.gov>
2026-07-16 09:03:53 -0500

    Test that data products from implicit providers are not sent to output

--- test/output_products.cpp
+++ test/output_products.cpp
@@ -6,7 +6,9 @@
 // =======================================================================================
 
 #include "phlex/core/framework_graph.hpp"
+#include "phlex/core/source.hpp"
 #include "phlex/model/data_cell_index.hpp"
+#include "phlex/model/products.hpp"
 #include "plugins/layer_generator.hpp"
 
 #include "catch2/catch_test_macros.hpp"
@@ -32,6 +34,35 @@ namespace {
   private:
     std::set<std::string>* products_;
   };
+
+  constexpr std::string brahms() { return "Brahms"; }
+
+  detail::product_ptr give_me_a_name(data_cell_index const&)
+  {
+    return detail::product_for(brahms());
+  }
+
+  class test_source : public detail::source {
+    detail::provider_bundles create_providers(product_selector const& selector) override
+    {
+      using namespace experimental;
+      using namespace phlex::detail;
+      provider_bundles bundles;
+      std::string const layer = "spill";
+      std::string const stage = "previous_process";
+      product_specification spec{"provide_name", "", make_type_id<std::string>()};
+
+      if (selector.match(spec, identifier{layer}, identifier{stage})) {
+        bundles.push_back(phlex::detail::provider_bundle{.provider_function = give_me_a_name,
+                                                         .max_concurrency = concurrency::unlimited,
+                                                         .spec = std::move(spec),
+                                                         .layer = layer,
+                                                         .stage = stage});
+      }
+      return bundles;
+    }
+    index_generator indices() override { co_return; }
+  };
 }
 
 TEST_CASE("Output data products", "[graph]")
@@ -41,6 +72,7 @@ TEST_CASE("Output data products", "[graph]")
 
   auto g = phlex::detail::framework_graph::without_driver();
   g.add_driver(gen);
+  g.add_source<test_source>("test_source");
 
   g.provide("provide_number", [](data_cell_index const&) -> int { return 17; })
     .output_product("input", "number_from_provider", "spill");
@@ -53,6 +85,12 @@ TEST_CASE("Output data products", "[graph]")
       product_selector{.creator = "input", .layer = "spill", .suffix = "number_from_provider"})
     .output_product_suffixes("squared_number");
 
+  // Create another node that requires a data product from an implicit provider.
+  // Concurrency must be serial because the CHECK macro cannot be invoked in a parallel context.
+  g.observe(
+     "read_name", [](std::string const& name) { CHECK(name == brahms()); }, concurrency::serial)
+    .input_family(product_selector{.creator = "provide_name", .layer = "spill"});
+
   std::set<std::string> products_from_nodes;
   g.make<product_recorder>(products_from_nodes)
     .output("record_numbers", &product_recorder::record, concurrency::serial);
@@ -61,10 +99,13 @@ TEST_CASE("Output data products", "[graph]")
 
   CHECK(g.execution_count("provide_number") == 1u);
   CHECK(g.execution_count("square_number") == 1u);
-  // The "record_numbers" output node should be executed twice: once to receive the data
-  // store from the "provide_number" provider, and once to receive the data store from the
-  // "square_number" transform.
-  CHECK(g.execution_count("record_numbers") == 2u);
-  CHECK(products_from_nodes ==
-        std::set<std::string>{"input/number_from_provider", "square_number/squared_number"});
+
+  // The "record_numbers" output node should be executed three times:
+  //   - once to receive the data store from the "provide_number" explicit provider,
+  //   - once to receive the data store from the "provide_name" implicit provider,
+  //   - once to receive the data store from the "square_number" transform.
+  CHECK(g.execution_count("record_numbers") == 3u);
+  CHECK(products_from_nodes == std::set<std::string>{"input/number_from_provider",
+                                                     "provide_name/",
+                                                     "square_number/squared_number"});
 }
