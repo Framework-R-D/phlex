0f8fc6113d6ff5ce189f0c1eca6ffd5f903ed470
Kyle Knoepfel <knoepfel@fnal.gov>
2026-07-28 16:51:19 -0500

    One more straggler

--- plugins/python/src/modulewrap.cpp
+++ plugins/python/src/modulewrap.cpp
@@ -557,7 +557,7 @@ namespace {
     }
 
     unsigned long ul = PyLong_AsUnsignedLong(pyobject);
-    if (ul == (unsigned long)-1 && PyErr_Occurred() && PyLong_Check(pyobject)) {
+    if (ul == static_cast<unsigned long>(-1) && PyErr_Occurred() && PyLong_Check(pyobject)) {
       PyErr_Clear();
       long i = PyLong_AS_LONG(pyobject);
       if (0 <= i) {
