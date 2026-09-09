9995a0228c26a2be657707019e147348b28fc14d
Andrew Olivier <andrew@djoinc.com>
2026-09-09 15:22:30 -0500

    Fix assert() condition that didn't compile in debug mode.

--- form/root_storage/root_rfield_write_container.cpp
+++ form/root_storage/root_rfield_write_container.cpp
@@ -116,7 +116,7 @@ namespace form::detail::experimental {
       }
     }
 
-    assert(rntuple_parent->get_model());
+    assert(rntuple_parent_->get_model());
     rntuple_parent_->get_model()->AddField(std::move(field));
   }
 }
