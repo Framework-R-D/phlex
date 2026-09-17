fffa610ec61834bf0a352c1ff1cf9ba4f7ef0eed
Wim Lavrijsen <WLavrijsen@lbl.gov>
2026-09-17 10:36:23 -0700

    make mypy shutup

--- test/python/adder.py
+++ test/python/adder.py
@@ -4,7 +4,7 @@ This test code implements the smallest possible run that does something
 real. It serves as a "Hello, World" equivalent for running Python code.
 """
 
-from typing import Protocol, TypeVar
+from typing import cast, Any, Protocol, TypeVar
 
 from phlex import Variant
 
@@ -19,7 +19,7 @@ class AddableProtocol[T](Protocol):
 Addable = TypeVar("Addable", bound=AddableProtocol)
 
 
-def add(i: Addable, j: Addable = 42) -> Addable:
+def add(i: Addable, j: Addable1 = cast(Any, 42)) -> Addable:
     """Add the inputs together and return the sum total.
 
     Use the standard `+` operator to add the two inputs together
