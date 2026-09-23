92c3dfab71cc3b97fe22dbd06269d95e3c1a837e
Andrew Olivier <andrew@djoinc.com>
2026-09-23 14:48:55 -0500

    Updated exception handling to also forward any information ROOT wants to propagate at CodeRabbit's suggestion.

--- form/root_storage/handle_rexception.cpp
+++ form/root_storage/handle_rexception.cpp
@@ -4,6 +4,6 @@
 
 namespace form::detail::experimental {
   void handle_rexception(std::string const& message, ROOT::RException const& e, std::source_location const loc) {
-    throw std::runtime_error(std::string(loc.function_name()) + ": " + message + " because:\n" + e.what());
+    std::throw_with_nested(std::runtime_error(std::string(loc.function_name()) + ": " + message + " because:\n" + e.what()));
   }
 }
